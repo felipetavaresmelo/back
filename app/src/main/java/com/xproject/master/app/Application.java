3cb40bf74846a848031c62a9a1894eaca8f48548
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2021-11-27 09:44:28 -0300

    Configurações de pacotes

--- app/src/main/java/com/xproject/master/app/Application.java
+++ app/src/main/java/com/xproject/master/app/Application.java
@@ -2,8 +2,10 @@ package com.xproject.master.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = "com.xproject.master")
 public class Application {
 
 	public static void main(String[] args) {
