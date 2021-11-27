dc749782686e06e9cbc2099daf852d1aaa38b263
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2021-11-27 01:54:58 -0300

    first commit

---
+++ app/src/test/java/com/xproject/master/app/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xproject.master.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
