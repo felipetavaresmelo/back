bf3ac1c67c7c46420e747dcb435365dcdf8258d7
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-01-15 01:15:55 -0300

    [#1] - Persistencia de Client

--- app/src/main/java/com/xproject/master/app/Application.java
+++ app/src/main/java/com/xproject/master/app/Application.java
@@ -3,7 +3,7 @@ package com.xproject.master.app;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication(scanBasePackages = "com.xproject.master")
+@SpringBootApplication(scanBasePackages = "com.xproject")
 public class Application {
 
     public static void main(String[] args) {
