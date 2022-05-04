317468558e93781a8fa6e629d978de0acca39cfc
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-05-04 17:40:20 -0300

    [#1] - getClientById

--- app/src/main/java/com/xproject/master/app/entrypoint/client/ClientRestController.java
+++ app/src/main/java/com/xproject/master/app/entrypoint/client/ClientRestController.java
@@ -10,7 +10,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.ObjectUtils;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
