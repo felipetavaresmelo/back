c767d424319c4d5e24588fac9d605dc8fb79f6cf
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-06-17 19:36:00 -0300

    #19 - Implantação da openApi

--- app/src/main/java/com/xproject/master/app/config/SwaggerConfig.java
+++ app/src/main/java/com/xproject/master/app/config/SwaggerConfig.java
@@ -1,37 +1,26 @@
 package com.xproject.master.app.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Tag;
-import springfox.documentation.spi.DocumentationType;
-import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class SwaggerConfig {
-    private static final String BASE_PACKAGE = "com.xproject.master.app.entrypoint";
-    public static final String TAG_ENTRY_POINT = "xPROJECT";
-    @Bean
-    public Docket api(){
-        return new Docket(DocumentationType.SWAGGER_2)
-                .select()
-                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
-                .paths(PathSelectors.any())
-                .build()
-                .apiInfo(getApiInfo());
-    }
-    private Tag getTagAppication(){
-        return new Tag(TAG_ENTRY_POINT, "Entrypoint to CRUD application.");
-    }
-    private ApiInfo getApiInfo() {
-        return new ApiInfoBuilder()
-                .title("X-Project")
-                .description("Project that explore Clean Architecture")
-                .version("1.0.0")
-                .build();
-    }
 
+    @Bean
+    public OpenAPI openApiConfiguration(
+            @Value("xProject") final String title,
+            @Value("0.0.1") final String version,
+            @Value("X Project") final String description
+    ) {
+        return new OpenAPI()
+                .info(new Info()
+                        .title(title)
+                        .version(version)
+                        .description(description)
+                        .termsOfService("Terms of service")
+                );
+    }
 }
