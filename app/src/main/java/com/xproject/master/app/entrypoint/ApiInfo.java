e17d1082d14641b47e7ca250693df01905c43a3b
Mario Camelo <mariuo@gmail.com>
2022-06-18 13:34:45 -0400

    Added Swagger springdoc-openapi

--- app/src/main/java/com/xproject/master/app/entrypoint/ApiInfo.java
+++ app/src/main/java/com/xproject/master/app/entrypoint/ApiInfo.java
@@ -5,9 +5,9 @@ import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.annotations.servers.Server;
 
 @OpenAPIDefinition(info =
-@Info(title = "Título"
-        , description = "descrição"
-        , version = "1.0.0"),
+@Info(title = "X-Project"
+        , description = "This project was made using Clean Archiquecture."
+        , version = "0.0.1"),
         servers = {
                 @Server(url = "http://localhost:8080/", description = "Enviromment Local"),
                 @Server(url = "http://10.10.10.10:8080/", description = "Enviromment Remoto")
