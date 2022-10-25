a466aac3a935734c64da2dddcc466463cf4bb1ed
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-10-25 02:04:50 -0300

    Order initiate developer

--- domain/src/main/java/com/xproject/master/domain/entity/order/Order.java
+++ domain/src/main/java/com/xproject/master/domain/entity/order/Order.java
@@ -1,7 +1,6 @@
 package com.xproject.master.domain.entity.order;
 
 import com.xproject.master.domain.entity.client.Client;
-import com.xproject.master.domain.entity.product.Product;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
