592d3ccb1b3631129fa950ca27f5efe1edfe3ea5
Mario Camelo <mariuo@gmail.com>
2022-11-23 16:25:32 -0500

    Update client TestContainer

--- app/src/test/java/com/xproject/master/app/entrypoint/client/ClientTestContainer.java
+++ app/src/test/java/com/xproject/master/app/entrypoint/client/ClientTestContainer.java
@@ -1,8 +1,8 @@
 package com.xproject.master.app.entrypoint.client;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.xproject.master.app.dto.AddressDto;
 import com.xproject.master.app.dto.ClientDto;
-import com.xproject.master.domain.entity.adreess.Address;
 import com.xproject.master.domain.entity.client.Client;
 import com.xproject.master.domain.usecase.adreess.ReadAddressByCepUseCase;
 import com.xproject.master.domain.usecase.client.CreateClientUseCase;
@@ -36,27 +36,16 @@ public class ClientTestContainer {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @InjectMocks
-    ClientRestController clientRestController;
-
     @Autowired
     private ObjectMapper objectMapper;
-    @Autowired
-    private ReadAddressByCepUseCase readAddressByCepUseCase;
-
-    @Mock
-    CreateClientUseCase createClientUseCase;
-
-    private ClientDto clientDTO;
     private List<ClientDto> clientDtoList;
     private Client client;
     private List<Client> clientList;
     private Long existingId;
     private Long existingId2;
     private Long nonExistingId;
-
-    private ClientDto clientDto1 = new ClientDto();
+    private ClientDto clientDto1;
+    private ClientDto clientDto2;
 
     @BeforeEach
     void setUp() {
@@ -68,12 +57,15 @@ public class ClientTestContainer {
 
         clientDto1.setName("Antonio Silva");
         clientDto1.setPhone("988888888");
-        Address address1 = new Address(readAddressByCepUseCase);
+        AddressDto address1 = new AddressDto();
         address1.setCep("51020090");
+        clientDto1.setAddress(address1);
 
-        ClientDto clientDto2 = new ClientDto();
+        clientDto2 = new ClientDto();
+        clientDto2.setId(existingId);
         clientDto2.setName("Brown");
         clientDto2.setPhone("222222222");
+        clientDto2.setAddress(address1);
         clientDtoList.add(clientDto2);
 
         clientList = new ArrayList<>();
@@ -114,7 +106,7 @@ public class ClientTestContainer {
                 .andExpect(jsonPath("$.id").value(existingId));
     }
     @Test
-    @DisplayName("Find should return NotFound 404 (Notfound) when id does not exists")
+    @DisplayName("Find should return NotFound 404 when id does not exists")
     void findByIdClientShouldReturnNotFoundWhenIdDoesNotExists() throws Exception{
         ResultActions result =
                 mockMvc.perform(MockMvcRequestBuilders.get("/client/{$id}", nonExistingId)
@@ -151,7 +143,34 @@ public class ClientTestContainer {
                         .contentType(MediaType.APPLICATION_JSON)
                         .accept(MediaType.APPLICATION_JSON));
         result.andExpect(status().isNotFound());
-
     }
+    @Test
+    @DisplayName("Update should return Status 200(Ok) and Client")
+    void updateClientShouldReturnStatusOkWhenValidData() throws Exception{
+
+        String jsonBody = objectMapper.writeValueAsString(clientDto2);
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.put("/client/{$id}", existingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(existingId))
+                .andExpect(jsonPath("$.name").value(clientDto2.getName()))
+                .andExpect(jsonPath("$.phone").value(clientDto2.getPhone()));
+    }
+    @Test
+    @DisplayName("Update should NotFound 404 when Id does not exists")
+    void updateClientShouldReturnNotFoundWhenIdDoesNotExists() throws Exception{
+
+        String jsonBody = objectMapper.writeValueAsString(clientDto2);
+        ResultActions result =
+                mockMvc.perform(MockMvcRequestBuilders.put("/client/{$id}", nonExistingId)
+                        .content(jsonBody)
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .accept(MediaType.APPLICATION_JSON));
+        result.andExpect(status().isNotFound());
+    }
+
 
 }
